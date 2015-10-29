69aed64b6b8ed77496a5a467822f8b44393bad3b
EugeneBuchkovsky <buchkovsyeugene20th@gmail.com>
2015-10-29 22:51:44 +0200

    v1

---
+++ Task1.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Task1;
+
+namespace Task1.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BookListService bl = new BookListService("booklist.txt");
+
+            bl.AddBook(new Book("Braun","Inferno",500,"Scince. Adventure"));
+            bl.AddBook(new Book("Zamyatin","We",100,"Dystopia"));
+            bl.AddBook(new Book("Tolkien", "The Lord Of The Rings", 1000, "Fantasy"));
+
+            System.Console.WriteLine(bl.ToString());
+            //bl.AddBook(new Book("Zamyatin", "Hronic", 134, "Fantasy"));
+            System.Console.WriteLine("__________________");
+            bl.SortBooksByTag();
+            System.Console.WriteLine(bl.ToString());
+
+            Book bok = bl.FindBookByTag(x => x.PagesAmount == 100);
+            System.Console.WriteLine(bok.ToString());
+
+            System.Console.ReadLine();
+        }
+    }
+}
